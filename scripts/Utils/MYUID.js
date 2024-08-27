851925d88910032c7968cfb2a2d85f4d188812e2
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-08-27 17:06:03 +0800

    add myuid

---
+++ scripts/Utils/MYUID.js
@@ -0,0 +1,29 @@
+class MYUID
+{
+    static size = 16;
+
+    static Generate() {
+        const array = new Uint8Array(MYUID.size);
+        crypto.getRandomValues(array);
+        return this._GenerateDataMarker_Internal(array);
+    }
+    static GenerateWithDataOnly()
+    {
+        const array = new Uint8Array(MYUID.size);
+        return this._GenerateDataMarker_Internal(array);
+    }
+
+    static _GenerateDataMarker_Internal(array)
+    {
+        const timestamp = Date.now();
+        const timestampBytes = new Uint8Array(new BigUint64Array([BigInt(timestamp)]).buffer);
+
+        for (let i = 0; i < timestampBytes.length - 2; i++) {
+            array[i] = timestampBytes[i];
+        }
+
+        return Array.from(array)
+            .map(value => value.toString().padStart(3, '0'))
+            .join('');
+    }
+}
